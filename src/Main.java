c81106987a68469a78454e9669c4c6fdba309f0c
Ignacio <qspectate@gmail.com>
2026-04-08 17:04:20 -0300

    refactor: mejora de legibilidad en la clase main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,43 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         Carrera sistemas = new Carrera();
 
-        Estudiante ignacio = new Estudiante("Miguel", "Bertola", 20, "Ing. Mecatronica", 7);
+        List estudiantes = new ArrayList<Estudiante>();
+
+        Estudiante ignacio = new Estudiante(
+                "Ignacio",
+                "Bertola",
+                19,
+                "Ing. Sistemas",
+                9.12
+        );
+
+        estudiantes.add(ignacio);
         sistemas.agregarEstudiante(ignacio);
 
-        Materia analisisMat = new Materia("Analisis Matematico", "129DF", 40);
+        Materia analisisMat = new Materia(
+                "Analisis Matematico",
+                "129DF",
+                40);
 
+        Materia algoritmos = new Materia(
+                "Algoritmos 2",
+                "1231D",
+                50
+        );
+
+        ignacio.agregarMateria(algoritmos);
         ignacio.agregarMateria(analisisMat);
 
-        System.out.println(ignacio.getCarrera());
+        System.out.println("Nombre: " + ignacio.getNombre());
+        System.out.println("Carrera: " + ignacio.getCarrera());
+
+        System.out.println("Materias:");
         ignacio.listarMaterias();
     }
 
